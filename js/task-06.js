2712c69e373a085141b389390126e674ed9f5fd7
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-11-11 16:14:41 -0700

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const input = document.querySelector("#validation-input");
 input.addEventListener("blur", onValid);
 
 function onValid(event) {
-  if (Number(input.dataset.length) <= event.currentTarget.value.length) {
+  if (Number(input.dataset.length) === event.currentTarget.value.length) {
     input.classList.add("valid");
     input.classList.remove("invalid");
   } else {
