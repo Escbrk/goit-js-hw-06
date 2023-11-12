e4236af8580b50e85add7743ab56300f8b2b4998
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-11-11 23:49:48 -0700

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  changeBtn: document.querySelector(".change-color"),
+  span: document.querySelector(".color"),
+  body: document.querySelector("body"),
+};
+
+refs.changeBtn.addEventListener("click", changeBcg);
+
+function changeBcg(event) {
+  refs.body.style.backgroundColor = getRandomHexColor()
+  refs.span.textContent = getRandomHexColor();
+}
