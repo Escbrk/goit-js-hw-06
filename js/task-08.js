1ff37a58b6403624db5588bf50f918234670de4f
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-11-11 23:25:10 -0700

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const refs = {
+  form: document.querySelector(".login-form"),
+};
 
+refs.form.addEventListener("submit", onSubmit);
+
+console.log();
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  refs.form.email.required = true;
+  refs.form.password.required = true;
+
+  refs.form.addEventListener("click", () => {
+    if (refs.form.email.value === "" || refs.form.password.value === "") {
+      alert("Все поля должны быть заполнены!");
+    } else {
+      const ObjectData = {
+        email: refs.form.email.value,
+        password: refs.form.password.value,
+      };
+
+      console.log(ObjectData);
+      refs.form.reset();
+    }
+  });
+}
